faedd8bf52f524db33b78efc3acf1b4aa0489e70
Akeelah <idristobi86@gmail.com>
2025-11-20 23:40:52 +0100

    commit

--- Scripts/Bronze/ddl_bronze.sql
+++ Scripts/Bronze/ddl_bronze.sql
@@ -6,7 +6,7 @@ Script Purpose:
     This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
 	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
+=============================================================================
 */
 
 
